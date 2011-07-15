666de75ccc50d2788df44ca11b72bb4d8aa82041
Anton Andriyevskyy <x.meglio@gmail.com>
2011-07-15 22:07:54 +0300

    Add meta.php file for autoupdates (json response)

---
+++ meta.php
@@ -0,0 +1,26 @@
+{
+"new_version": "0.1",
+"url": "https://github.com/meglio/wp-upgrademe",
+"package": "https://github.com/downloads/meglio/wp-upgrademe/upgrademe-0.1.zip",
+"info": {
+"name": "upgrademe",
+"author": "<a href='https://github.com/meglio'>Meglio</a>",
+"author_profile": "https://github.com/meglio",
+"requires": "3.2",
+"tested": "3.2.1",
+"rating": 98,
+"num_ratings": 680,
+"downloaded": 1950,
+"last_upated": "2011-07-15",
+"added": "2011-07-15",
+"homepage": "https://github.com/meglio/wp-upgrademe",
+"tags": {"wordpress":"wordpress","upgrade":"upgrade","update":"update","wp":"wp","repository":"repsitory","plugins":"plugins","hacks":"hacks","trucks":"trucks"},
+"sections":{
+"description": "Allows auto-upgrade for plugins outside of official WordPress repository",
+"installation": "As simple as 2 steps: extract in plugins folder, activate in administration panel",
+"screenshots":"",
+"changelog":"",
+"faq":""
+}
+}
+}
